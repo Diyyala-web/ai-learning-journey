0d78c4d17c3bf82915fb6c77ccd5cf8a2db2a7a7
Diyyala Archana <diyyala.archana95@gmail.com>
2025-10-02 15:30:41 +0530

    Day1: My first Python program

---
+++ Day-01/first_pgm.py
@@ -0,0 +1,29 @@
+#My first Program
+#Date: Oct 2, 2025
+#Jouney: Tramsition to AI/ML Engineer
+
+print("="*50)
+print("Hello, AI Journey!")
+print("="*50)
+
+#Taking user input
+name=input("What's Your name? ")
+print(f"\n Welcome {name}!")
+
+#Simple Calculations
+print(f"\n{name}, let me show you some Python magic:")
+print(f"5+3 = {5+3}")
+print(f"10*4 = {10*4}")
+print(f"20/4 = {20/4}")
+print(f"15-7 = {15-7}")
+
+#Your commitment
+print(f"\n{name}, you're starting an amazing journeu!")
+print(f"Next 12 months will tranform your life.")
+print("Let's build your AI future together! 🚀")
+
+#Small calculations with age
+age= int(input("\n what's your age?"))
+birth_year=2025-age
+print(f"You were born around {birth_year}")
+print(f"In 5 years (2030), you'll be {age+5} and earning 10X your current salary!")
